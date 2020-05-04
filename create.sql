f899feb10db9ef00ace26e0ee873132a0c9add85
Kseniia <44757008+KsyuYu@users.noreply.github.com>
2020-05-04 22:04:16 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,18 +1,14 @@
-CREATE TABLE Countries(
-country VARCHAR(30) NOT NULL,
-total_cases INT NOT NULL REFERENCES Rank (total_cases)
-);
+CREATE TABLE TotalCases (
+    country VARCHAR(30) NOT NULL
+    ,total_cases INT NOT NULL REFERENCES Rank (total_cases)
+    ,new_cases INT NOT NULL
+    );
 
-CREATE TABLE Cases(
-total_cases VARCHAR2 (30) NOT NULL,
-new_cases INT NOT NULL,
-total_deaths INT NOT NULL,
-data DATE);
+CREATE TABLE TotalDeaths (
+    country VARCHAR(30) NOT NULL
+    ,total_deaths INT NOT NULL
+    ,new_deaths INT NOT NULL
+    );
 
-CREATE TABLE Disease(
-disease VARCHAR2 (50) NOT NULL,
-data DATE);
-
-ALTER TABLE Countries ADD CONSTRAINT Countries_PK PRIMARY KEY (country);
-ALTER TABLE Cases ADD CONSTRAINT Cases_PK PRIMARY KEY (total_cases);
-ALTER TABLE Disease ADD CONSTRAINT Disease_PK PRIMARY KEY (data);
+ALTER TABLE TotalCases ADD CONSTRAINT TotalCases_PK PRIMARY KEY (country);
+ALTER TABLE TotalDeaths ADD CONSTRAINT TotalDeaths_PK PRIMARY KEY (country);
