14700e00a2360c5dbe2828fc54765499810f2d55
Kseniia <44757008+KsyuYu@users.noreply.github.com>
2020-05-04 22:14:52 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,13 +1,10 @@
 CREATE OR REPLACE VIEW Total AS
     SELECT 
-        Countries.country, 
-        Cases.total_cases,
-        Cases.new_cases,
-        Cases.total_deaths,
-        Cases.data,
-        Disease.disease
+        TotalCases.country, 
+        TotalCases.total_cases,
+        TotalCases.new_cases,
+        TotalDeaths.total_deaths,
+        TotalDeaths.data
     FROM
-        Countries JOIN Cases
-        ON Countries.total_cases = Cases.total_cases
-        JOIN Disease
-        ON Disease.data = Cases.data;
+        TotalCases JOIN TotalDeaths
+        ON TotalCases.country = TotalCases.country;
